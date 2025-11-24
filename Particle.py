1cd045ccc6263cb4161b740c3ac2a43efd60b3f1
Sendy45 <itamarsan45@gmail.com>
2025-11-24 23:27:06 +0200

    Final commit - comments and cleaning up

--- Particle.py
+++ Particle.py
@@ -1,18 +1,17 @@
 import pygame
 
-
 class Particle:
     def __init__(self, position, color):
         self.position = position
         self.color = color
-        self.mass = 1 #9.109 *  10**-31
         self.velocity = [0, 0]
-        self.acceleration = [0, 0]
         self.radius = 5
 
+    # Draw particle
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, self.position, self.radius)
 
+    # Handle wall collision - prevent out of bounds coordinates
     def wall_collision(self, screen):
         if self.position[0] < 0 or self.position[0] > screen.get_width():
             self.velocity[0] *= -1
--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 import sys
 import random
-
 import pygame
 from Particle import Particle
 
+# Particle forces - attraction or repelling forces
 def rule(particles1, particles2, g, screen):
     for p1 in particles1:
         fx = 0
         fy = 0
         for p2 in particles2:
+            # distance between particles
             dx = p1.position[0] - p2.position[0]
             dy = p1.position[1] - p2.position[1]
             d = (dx * dx + dy * dy) ** 0.5
-            if 0 < d < 80:
-                F = g * (1/d)
+            if 0 < d < 80: # disregarding small forces
+                F = g * (1/d) # non accurate force calculation
+                # per axis force
                 fx += F * dx
                 fy += F * dy
 
+        # Update velocities per axis and position
+        # cut velocity by half - smoother animation
         p1.velocity[0] = (p1.velocity[0] + fx) * 0.5
         p1.velocity[1] = (p1.velocity[1] + fy) * 0.5
         p1.position[0] += p1.velocity[0]
         p1.position[1] += p1.velocity[1]
 
+        # Handle wall collision
         p1.wall_collision(screen)
 
+# Main  simulation function
 def run_simulation():
+    # Initialization
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    clock = pygame.time.Clock()
     running = True
 
-    fps = 30
-
+    # Particle groups - sizes and colors
     reds = [
         Particle([random.randint(0, 800), random.randint(0, 600)], "red")
         for _ in range(200)
@@ -48,17 +53,16 @@ def run_simulation():
     particles = reds + blues + greens
 
     while running:
+        # Exist handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
                 sys.exit()
 
-        for particle in particles:
-            particle.acceleration = [0, 0]
-
         screen.fill((0, 0, 0))
 
+        # Set of rules - changeable
         rule(greens, greens, -0.32, screen)
         rule(greens, reds, -0.17, screen)
         rule(greens, blues, 0.34, screen)
@@ -69,6 +73,7 @@ def run_simulation():
         rule(blues, blues, 0.15, screen)
         rule(blues, greens, -0.20, screen)
 
+        # Draw particles
         for particle in particles:
             particle.draw(screen)
 
